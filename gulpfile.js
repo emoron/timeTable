779a826020014879e8c2d20421e0732f8df223f2
Edgardo <emoron@gmail.com>
2016-04-27 10:28:07 -0500

    update

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+// Include gulp
+var gulp = require('gulp');
+
+ // Include plugins
+var concat = require('gulp-concat');
+ // Concatenate JS Files
+
+var jade = require('gulp-jade');
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+      .pipe(concat('generador.js'))
+      .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('jade',function(){
+  return gulp.src('src/index.jade')
+  .pipe(jade())
+  .pipe(gulp.dest('build'))
+});
+gulp.task('watch', function() {
+  gulp.watch('src/*.jade',['jade']);
+   // Watch .js files
+  gulp.watch('src/js/*.js', ['scripts']);
+   // Watch .scss files
+  gulp.watch('src/scss/*.scss', ['sass']);
+   // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+ });
+
+ // Default Task
+gulp.task('default', ['scripts','watch']);
